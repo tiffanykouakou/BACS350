416cce7a486d3ac6ad923c0d7abad28c128eab02
tiffanykouakou <tiffanykouakou@gmail.com>
2021-08-26 17:11:22 -0600

    Configure Hello app

--- week1/Hello/config/urls.py
+++ week1/Hello/config/urls.py
@@ -19,3 +19,10 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.urls import path
+from pages.views import homePageView
+
+urlpatterns = [
+    path('', homePageView)
+]

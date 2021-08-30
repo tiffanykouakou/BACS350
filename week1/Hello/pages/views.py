161d9f85a710673fec1a0d3561648f219e76f0aa
tiffanykouakou <tiffanykouakou@gmail.com>
2021-08-30 16:12:05 -0600

    Configure Hello app

--- week1/Hello/pages/views.py
+++ week1/Hello/pages/views.py
@@ -5,5 +5,3 @@ from django.http import HttpResponse
 
 def homePageView(request):
     return HttpResponse('Hello World')
-    return HttpResponse('<h1>Hello There</h1><p>This is cool.</p>')
-    return HttpResponse('<h1>This is a test</h1>" '<p>I'm trying to think but nothing happens</p>')

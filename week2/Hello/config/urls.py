893ee787bf00cbee804f3ba9256f02c0b808440b
tiffanykouakou <tiffanykouakou@gmail.com>
2021-09-03 22:32:19 -0600

    superhero

--- week2/Hello/config/urls.py
+++ week2/Hello/config/urls.py
@@ -1,7 +1,9 @@
-
+from week2.Hello.pages.views import HomeView
 from django.urls import path
 from pages.views import AboutView
 
 urlpatterns = [
     path('', AboutView.as_view()),
+    path('about', AboutView.as_view()),
+    path('home', HomeView.as_view()),
 ]

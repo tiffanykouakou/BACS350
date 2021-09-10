bdecdb197df417a41427d545cc1f42a7253419ac
tiffanykouakou <tiffanykouakou@gmail.com>
2021-09-10 15:05:29 -0600

    commit

--- week3/Superhero/config/settings.py
+++ week3/Superhero/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-u*+-*+%w%av13%7othzql59r8u)kw6rdk+mt*uv4f(#oifjo%x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tiffanykouakou.pythonanywhere.com']
 
 
 # Application definition

c1e61cfd0cf8b69666e098979a9a7ce8f88f036e
tiffanykouakou <tiffanykouakou@gmail.com>
2021-09-20 18:09:33 -0600

    changes

--- week4/Superhero/config/urls.py
+++ week4/Superhero/config/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from hero.views import IndexView, HeroListView, HeroDetailView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', IndexView.as_view()),
     path('', IndexView.as_view()),
     path('hero/', HeroListView.as_view()),
     path('hero/<int:pk>', HeroDetailView.as_view()),
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
